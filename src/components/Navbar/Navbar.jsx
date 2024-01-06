905bc6a6da1142c958ac3a6e88ba13c028f4f3da
MrKalyanKing <badhavathkalyan08@gmail.com>
2024-01-06 17:34:01 +0530

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,37 @@
-import React, { useContext, useState } from 'react'
+
+import React, { useContext, useState,useRef } from 'react'
 import './Navbar.css'
 import logo_nav from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../Context/ShopContext'
+import nav_dropdown from '../Assets/dropdown.png'
+//import 'bootstrap/dist/css/boootstrap/.min.css'
+//import {Container } from 'reract-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 export const Navbar = () => {
   const[menu,setmenu]=useState("shop")
   const {getTotalCArtitem}=useContext(ShopContext)
+  const menuRef= useRef();
+  const dropdown_toggle=(e)=>{
+        menuRef.current.classList.toggle('navmenu-visible');
+        e.target.classList.toggle('open');
+  }
   return (
+    
+      
     <div className='Navbar'>
+      
+    
         <div className="nav-logo">
-          <img src={logo_nav} alt="" />
+          <img  src={logo_nav} alt="" />
             <h1>SHOPPER</h1>
         </div>
-            <div className="navmenu">
+      
+             <img className='nav_dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+            <div ref={menuRef} className="navmenu">
                 <ul className='kh'>
                     <li onClick={()=>{setmenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{(menu==="shop"?<hr/>:<></>)}</li>
                     <li onClick={()=>{setmenu("men")}}><Link style={{textDecoration:'none'}} to='/men'>Men</Link>{(menu==="men"?<hr/>:<></>)}</li>
@@ -22,12 +39,16 @@ export const Navbar = () => {
                     <li onClick={()=>{setmenu("kids")}}><Link style={{textDecoration:'none'}}to='/kids'>Kids</Link>{(menu==="kids"?<hr/>:<></>)}</li>
                 </ul>
             </div>
+      
+      
             <div className="navcart">
               <Link to='loginsignup'><button className='btn'>Login</button></Link>
               <Link to='Cart'><img src={cart_icon} alt="" /></Link>
               <div className="counter">{getTotalCArtitem()}</div>
             </div>
     </div>
+    
+  
   )
   }
-export default Navbar
+export default Navbar 
--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Offer.css'
 import offer_img from '../Assets/exclusive_image.png'
-import arrow_img from '../Assets/arrow.png'
+//mport arrow_img from '../Assets/arrow.png'
 
 const Offer = () => {
   return (
@@ -10,8 +10,8 @@ const Offer = () => {
         <h1>Exclusive </h1>
         <h1>Offers For You</h1>
         <p>ONLY FROM BEST SELLERS PRODUCTS</p>
-        <button>Check Now</button>
-        <img src={arrow_img} alt="" />
+        <button><p> Check Now</p></button>
+        
     </div>
       <div className="offer-right">
         <img src={offer_img} alt="" />
